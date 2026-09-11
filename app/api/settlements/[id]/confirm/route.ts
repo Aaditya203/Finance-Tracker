c2fc44a0cff24b20c5507611fb3c61ccaf76110d
Aditya Sharma <adityas20032005@gmail.com>
2026-09-11 11:12:40 +0530

    Changed the Settlement to trust from userID sent from body to auth

--- app/api/settlements/[id]/confirm/route.ts
+++ app/api/settlements/[id]/confirm/route.ts
@@ -1,15 +1,18 @@
 import { SettlementStatus } from "@/app/generated/prisma/enums";
+import { requiredAuth } from "@/lib/auth-service";
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function PATCH(request:Request,{params}:{params:Promise<{id:string}>}){
    try{
+        const { userId } = await requiredAuth();
        const {id:settlementId} = await params;
-       const body = await request.json();
-        const {userId} = body;
+
+       
         if(!userId){
-            return NextResponse.json({error:"User Id is Required!"},{status:400});
+            return NextResponse.json({error:"Not Logged In!"},{status:400});
         }
+
         const settlement = await prisma.settlement.findUnique({
             where:{
                 id:settlementId,
@@ -27,34 +30,27 @@ export async function PATCH(request:Request,{params}:{params:Promise<{id:string}
                 error:"You are not authorized to confirm this settlement!",
             },{status:400})
         }
-
+        if(settlement.status !== SettlementStatus.PENDING){
+            return NextResponse.json({error:"Only Pending Settlements Can Be Confirmed"},{status:400});
+        }
         const updatedSettlement = await prisma.settlement.update({
-            where:{
-                id:settlementId
-            },
+            where:{id:settlementId},
             data:{
                 status:SettlementStatus.COMPLETED,
                 settledAt:new Date()
             },
             include:{
-                fromUser:{
-                    select:{
-                        id:true,
-                        name:true
-                    }
-                },
-                toUser:{
-                    select:{
-                        id:true,
-                        name:true
-                    }
-                },
+                fromUser:{select:{id:true,name:true}},
+                toUser:{select:{id:true,name:true}},
                 attachments:true
             }
         });
         return NextResponse.json({message:"Settlement confirmed successfully",updatedSettlement})
    }
    catch(error){
+    if(error instanceof Error && error.message === "UNAUTHORIZED"){
+        return NextResponse.json({error:"Unauthorized"},{status:401})
+    }
     console.log(error);
     return NextResponse.json({error:"Failed to confirm settlement"},{status:500})
    }
